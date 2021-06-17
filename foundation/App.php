093c46b5d28a25e784921b4acdbd14c0a3e0ffb5
Mathieu Margotin <mathieu.margotin@gmail.com>
2021-06-17 14:46:00 +0200

    create main class App.php

---
+++ foundation/App.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nitogram\Foundation;
+
+class App
+{
+    public function __construct()
+    {
+    }
+
+    public function render(): void
+    {
+        echo "Hello !";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
+use Nitogram\Foundation\App;
+
 define("ROOT", \dirname(__DIR__));
 
 require_once ROOT . "/vendor/autoload.php";
+
+$app = new App();
+$app->render();
