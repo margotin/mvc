a42aec3ac1b8f47066183937d98c093cae0efa80
Mathieu Margotin <mathieu.margotin@gmail.com>
2021-06-17 14:49:12 +0200

    init dotenv

--- foundation/App.php
+++ foundation/App.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace Nitogram\Foundation;
 
+use Dotenv\Dotenv;
+
 class App
 {
     public function __construct()
     {
+        $this->initDotenv();
+    }
+
+    private function initDotenv():void
+    {
+        $dotenv = Dotenv::createImmutable(ROOT);
+        $dotenv->safeLoad();
     }
 
     public function render(): void
